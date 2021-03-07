f452a65fb32c9f7da9cdab55398e23dcf5df99c2
kensaku komatsu <kensaku.komatsu@gmail.com>
2021-03-07 17:00:50 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
+import OBSWebScoket from 'obs-websocket-js'
 import './App.css';
 
+const PASSWORD = process.env.REACT_APP_PASSWORD
+
 function App() {
+  useEffect(() => {
+    ( async () => {
+      const obs = new OBSWebScoket()
+      const password = PASSWORD || ''
+
+      await obs.connect({
+        address: 'localhost:4444',
+        password
+      }).catch( err => {throw err})
+
+      console.log( 'succeeded connecting to obss')
+
+      const sceneList = await obs.send('GetSceneList')
+      console.log( 'current-scene:', sceneList.currentScene )
+      sceneList.scenes.forEach( (scene, idx) => {
+        console.log( `[${idx}] ${scene.name}` )
+      })
+    })()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>kokutele OBS control</h1>
       </header>
     </div>
   );
